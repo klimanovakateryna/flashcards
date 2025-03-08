a39278fd282e16eee24d0e9ebc3fa372db8f5aaa
klimanovakateryna <klimanovakateryna@gmail.com>
2025-03-07 21:44:39 -0500

    Add all required features

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import FlashCard from './components/FlashCard';   
+
+const App = () => {
+
+  return (
+
+    <div className="App">
+      <FlashCard /> 
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/FlashCard.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "./FlashCard.css";
+
+const FlashCard = () => {
+  const cards = [
+    { img: '/pictures/space-invaders.jpg', answer: 'Space Invaders' },
+    { img: '/pictures/super-mario.jpg', answer: 'Super Mario' },
+    { img: '/pictures/pong.webp', answer: 'Pong' },
+    { img: '/pictures/breakout.webp', answer: 'Breakout' },
+    { img: './pictures/pacman.webp', answer: 'PacMan' }
+  ];
+
+  const [isFlipped, setIsFlipped] = React.useState(false);
+  const [index, setIndex] = React.useState(0);
+
+  const nextCard = () => {
+    let randomIndex = Math.floor(Math.random() * cards.length);
+    if (randomIndex === index) {
+      randomIndex = (index + 1) % cards.length;
+    }
+    setIsFlipped(false);
+    setIndex(randomIndex);
+  };
+
+  const prevCard = () => {
+    setIsFlipped(false);
+    setIndex((index - 1 + cards.length) % cards.length);
+  };
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  const currentCard = cards[index];
+
+  return (
+    <>
+      <div className="header">
+        <div className="text-container">
+        <h1 className="title">GUESS RETRO GAME</h1>
+        <p className="description">
+          Test your retro gaming knowledge by guessing the classic arcade game from its image.
+        </p>
+      
+        <p className="card-info">Total Cards: {cards.length}</p>
+        </div>
+      </div>
+      <div className="card-container" onClick={handleClick}>
+        <div className={`card ${isFlipped ? "flipped" : ""}`}>
+          <div className="card-inner">
+            <div className="card-front">
+              <img src={currentCard.img} alt="Game Front" className="image" />
+            </div>
+            <div className="card-back">
+              <h1>{currentCard.answer}</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="button-container">
+        <button onClick={prevCard} disabled={index === 0}>←</button>
+        <button onClick={nextCard} disabled={index === cards.length - 1}>→</button>
+      </div>
+    </>
+  );
+};
+
+export default FlashCard;
